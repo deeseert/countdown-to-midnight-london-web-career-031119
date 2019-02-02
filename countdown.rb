3f76a38f62266f401ea70ea7b04713a9691d0b79
deeseert <gio.amms@gmail.com>
2019-02-02 19:15:44 +0100

    Done.

--- countdown.rb
+++ countdown.rb
@@ -12,6 +12,11 @@ end
 
 
 def countdown_with_sleep(number)
-  countdown(number)
-  sleep(5)
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(5)
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
